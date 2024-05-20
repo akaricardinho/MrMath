837fa33d0f92b01ca2e580e7875db06a4161e556
Ricaro <Usuario@CPD01-M06>
2024-05-20 15:25:59 -0300

    Design de SignPage concluído

---
+++ MrMath/Views/SignPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace MrMath.Views;
+
+public partial class SignPage : ContentPage
+{
+	public SignPage()
+	{
+		InitializeComponent();
+        AppShell.SetNavBarIsVisible(this, false);
+    }
+}
--- MrMath/Views/profilePage.xaml.cs
+++ MrMath/Views/profilePage.xaml.cs
@@ -7,4 +7,9 @@ public partial class profilePage : ContentPage
 		InitializeComponent();
         AppShell.SetNavBarIsVisible(this, false);
     }
+
+    private void logoutButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Views.SignPage());
+    }
 }
