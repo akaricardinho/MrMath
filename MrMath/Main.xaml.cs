2539a39eb29ee71a1a975d733292810ce8293bb9
Ricardo <Usuario@CPD03-M05>
2024-08-05 12:13:56 -0300

    Início das telas de login

---
+++ MrMath/Main.xaml.cs
@@ -0,0 +1,14 @@
+namespace MrMath;
+
+public partial class Main : ContentPage
+{
+	public Main()
+	{
+		InitializeComponent();
+	}
+
+    private void debugButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
